c5e877bdc37e8ae23431a7d963497a2f3c4dfa9a
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-19 20:55:54 +0300

    Update to match checker requirements

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
--- Use the alx_book_store database
+-- Use the correct database
 USE alx_book_store;
 
--- Get the full description of the Books table without using DESCRIBE, EXPLAIN, or ANALYZE
+-- Fetch full table description without DESCRIBE or ANALYZE
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'

c261fd18e6951f8862766cf13c121dd751fb525b
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-19 20:52:45 +0300

    Add task_4.sql to fetch full description of Books table via INFORMATION_SCHEMA

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,8 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Display full description of the Books table without using DESCRIBE, EXPLAIN, or ANALYZE
-SELECT COLUMN_NAME, COLUMN_TYPE
+-- Get the full description of the Books table without using DESCRIBE, EXPLAIN, or ANALYZE
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
   AND TABLE_NAME = 'Books';

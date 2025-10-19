161feef33e8be89c17775706790ef04cc4a8a15d
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-19 20:33:28 +0300

    Handle MySQL exceptions using 'except mysql.connector.Error' as required by ALX checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 from dotenv import load_dotenv
 import os
 
@@ -20,7 +19,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
 
     finally:

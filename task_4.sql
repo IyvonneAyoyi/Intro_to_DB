f06a576cc69f3dde18ef8fbd8e74af7411b2842c
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-19 21:03:43 +0300

    Add TABLE_SCHEMA to INFORMATION_SCHEMA query for Books table description

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
--- Full description of the table Books
+-- Full description of Books table from alx_book_store database
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
+WHERE TABLE_SCHEMA = 'alx_book_store'
   AND TABLE_NAME = 'Books';

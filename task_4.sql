4cbcd5a061d4202f6126d55febcd372db90eab5c
IyvonneAyoyi <iyvonneayoyi@gmail.com>
2025-10-19 20:51:18 +0300

    Add task_4.sql to describe Books table using INFORMATION_SCHEMA.COLUMNS

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Retrieve full description of the Books table without using DESCRIBE or EXPLAIN
+-- Display full description of the Books table without using DESCRIBE, EXPLAIN, or ANALYZE
 SELECT COLUMN_NAME, COLUMN_TYPE
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
